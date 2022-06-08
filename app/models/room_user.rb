03dc6b5dda1c731eab6d39ffe8047b0dd1f36fc5
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-06-09 00:01:28 +0900

    間違い

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,4 @@
 class RoomUser < ApplicationRecord
-    class RoomUser < ApplicationRecord
-        belongs_to :room
-        belongs_to :user
-    end
-     
-end
+    belongs_to :room
+    belongs_to :user
+  end

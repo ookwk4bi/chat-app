3acb715e3b111e326d81507c7dcec5b1b88040dc
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-06-10 12:14:47 +0900

    各モデルのテスト

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,3 +46,5 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
+
+config.active_job.queue_adapter = :inline
